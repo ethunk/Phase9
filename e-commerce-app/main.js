449fee74bc5c7e226f8bcf77d91ade09a8c82512
ethunk <ethunk@gmail.com>
2017-10-22 21:10:24 -0400

    finished the challenge

--- e-commerce-app/main.js
+++ e-commerce-app/main.js
@@ -7,18 +7,18 @@ let inventory = [
 // your code, here
 
 class Product {
-  contructor(id, quantity, name, url, description) {
+  constructor(id, quantity, name, url, description) {
     this.id = id;
     this.quantity = quantity;
     this.name = name;
     this.url = url;
     this.description = description;
-    debugger
   }
 
   sell() {
-    debugger
-    --this.quantity;
+    if (this.quantity > 0) {
+      this.quantity -= 1;
+    }
     return (this.quantity);
   }
   toHTML() {
@@ -29,35 +29,28 @@ class Product {
     let img = document.createElement('IMG');
     let h3Description = document.createElement('h3');
     let pDescription = document.createElement('p');
-    debugger
     //setting attributes
     mainDiv.className = 'product';
-    h1Product.innerHTML = 'Product Name: ${this.name}';
+    h1Product.innerHTML = `Product Name: ${this.name}`;
     h5Quantity.innerHTML = `${this.quantity} In Stock`;
     img.src = `${this.url}`;
     h3Description.innerHTML = 'Description';
-    pDescription.innerHTML = '${this.description}';
+    pDescription.innerHTML = `${this.description}`;
     //appending together
-    h3Description.appendChild(pDescription);
-    mainDiv.appendChild(h1Product,h5Quantity, img, h3Description);
+    [h1Product, h5Quantity, img, h3Description, pDescription].forEach(function(element) {
+        mainDiv.appendChild(element);
+      }
+    );
     return (mainDiv);
   }
 }
 
+let products = inventory.map(function(productData) {
+  return new Product(...productData);
+});
 
-class Person {
-  constructor(name) {
-    this.name = name;
-  }
+let element = document.getElementById('all-products');
 
-  greet() {
-    debugger
-    return `Hello, my name is ${this.name}.`;
-  }
-}
-
-let john = new Person('John');
-console.log(john.greet());
-
-
-let test = new Product(453, 50, 'Toy Test', 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png', 'some type of description');
+products.forEach((product) => {
+  element.appendChild(product.toHTML());
+});
