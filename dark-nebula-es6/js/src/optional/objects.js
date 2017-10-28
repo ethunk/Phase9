9967ae6078ebcb4f17e0e417df7f228106ec75cd
ethunk <ethunk@gmail.com>
2017-10-27 21:08:55 -0400

    all object specs are now passing

--- dark-nebula-es6/js/src/optional/objects.js
+++ dark-nebula-es6/js/src/optional/objects.js
@@ -1,11 +1,19 @@
 alterContext = (fn, obj) => {
-  // Optional Challenge
+  return fn.bind(obj)();
+  //.bind(obj), passes the obj to the function fn, as the this object.
 };
 
-alterObjects = (constructor, greeting) => {
-  // Optional Challenge
+alterObjects = (obj, greeting) => {
+  obj.prototype.greeting = greeting;
+  //.prototype adds attributes or functions to an object
 };
 
 iterate = (obj) => {
-  // Optional Challenge
+    let propertiesNames = Object.getOwnPropertyNames(obj);
+    let propertiesValues = Object.values(obj);
+    let results = [];
+    for (i = 0; i < propertiesNames.length; i++) {
+      results.push(`${propertiesNames[i]}` + ": " + `${propertiesValues[i]}`);
+    }
+    return results;
 };
