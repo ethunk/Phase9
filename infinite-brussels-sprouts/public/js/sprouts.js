cddb886e074959e7a61546c5015b0128b244a69d
ethunk <ethunk@gmail.com>
2017-11-10 19:32:54 -0500

    completed the optional challenge

--- infinite-brussels-sprouts/public/js/sprouts.js
+++ infinite-brussels-sprouts/public/js/sprouts.js
@@ -1,10 +1,8 @@
 // YOUR CODE GOES HERE
-
-let tweetText = $('.tweets');
-
 let pageNumber = () => {
   let regex = /^d+$/;
-  if (regex.test($('a').attr('href').split('').pop())){
+  let isNumber = regex.test($('a').attr('href').split('').pop());
+  if (isNumber){
     return $('a').attr('href').split('').pop();
   } else {
     return '0';
@@ -13,20 +11,54 @@ let pageNumber = () => {
 
 let page = parseInt(pageNumber());
 
+let tweetDataRequest = $.ajax({
+  method: 'GET',
+  url: '/tweets.json?page=' + `${page} +1`
+});
+
 let linkButton = $('.more-sprouts').on('click', (event) => {
   event.preventDefault();
+
   page +=1;
-  let request = $.ajax({
+
+  let tweetDataRequest = $.ajax({
     method: 'GET',
     url: '/tweets.json?page=' + `${page} +1`
   });
-  request.done((data) => {
+
+  tweetDataRequest.done((data) => {
     for (i=0; i < data.length; i++) {
       htmlText ='';
       htmlText +='<li class="tweet">';
-      htmlText +='<div class="body">'+  `${data[i]["text"]}`+'</div>';
-      htmlText +='<div class="user">'+`${data[i]['username']}`+'</div></li>';
-      tweetText.append(htmlText);
+      htmlText +='<div class="body">'+  `${data[i].text}`+'</div>';
+      htmlText +='<div class="user">'+`${data[i].username}`+'</div></li>';
+      $('.tweets').append(htmlText);
     }
   });
 });
+
+window.onscroll = function(event) {
+  // console.log(window.innerHeight);
+  // console.log(window.scrollY);
+  let documentHeight = () => {return $(document).height();};
+  let viewportSize = window.innerHeight;
+  let topOfScreenHeight = () => {return window.scrollY;};
+
+  if ((viewportSize + topOfScreenHeight()) >= (documentHeight())) {
+    page +=1;
+    let tweetDataRequest = $.ajax({
+      method: 'GET',
+      url: '/tweets.json?page=' + `${page} +1`
+    });
+
+    tweetDataRequest.done((data) => {
+      for (i=0; i < data.length; i++) {
+        htmlText ='';
+        htmlText +='<li class="tweet">';
+        htmlText +='<div class="body">'+  `${data[i].text}`+'</div>';
+        htmlText +='<div class="user">'+`${data[i].username}`+'</div></li>';
+        $('.tweets').append(htmlText);
+      }
+    });
+  }
+};
